040ad03b3f21fd2c9acc0d58beceb2417f3073ce
vajapu <37873587+vajapu@users.noreply.github.com>
2020-09-26 21:53:13 +0530

    Update scripts.sh

--- pipelines/scripts.sh
+++ pipelines/scripts.sh
@@ -26,7 +26,7 @@ start_android_emulator(){
   $ANDROID_HOME/emulator/emulator -list-avds
   echo "Starting emulator"
   # Start emulator in background
-  nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
+  nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot -no-boot-anim > /dev/null 2>&1 &
   $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
   $ANDROID_HOME/platform-tools/adb devices
   echo "Emulator started"
