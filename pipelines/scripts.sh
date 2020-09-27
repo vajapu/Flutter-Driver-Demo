f8c6c74974a2a05544fa4f2f3547086b4c41268c
vajapu <37873587+vajapu@users.noreply.github.com>
2020-09-27 09:49:07 +0530

    Update scripts.sh

--- pipelines/scripts.sh
+++ pipelines/scripts.sh
@@ -34,7 +34,7 @@ start_android_emulator(){
 
 generate_test_report() {
   npm install multiple-cucumber-html-reporter
-  node test_driver/pipelines/reporter.js
+  node pipelines/reporter.js
 }
 
 "$@"
